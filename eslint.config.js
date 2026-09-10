7054db12f57385af839b991621026d5a64ccbeec
luckwizera <kwizerajeanluc18@gmail.com>
2026-09-10 13:28:53 +0200

    chore: align database ignore path with Discipline

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,16 @@
 import js from '@eslint/js';
 import globals from 'globals';
+
 export default [
-  { ignores: ['node_modules/**', 'server/ecard.sqlite', 'dist/**'] },
+  { ignores: ['node_modules/**', 'server/discipline.sqlite', 'dist/**'] },
   js.configs.recommended,
-  { files: ['**/*.js'], languageOptions: { ecmaVersion: 'latest', sourceType: 'module', globals: { ...globals.node, ...globals.browser } }, rules: { 'no-unused-vars': ['error', { argsIgnorePattern: '^_' }] } }
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.node, ...globals.browser }
+    },
+    rules: { 'no-unused-vars': ['error', { argsIgnorePattern: '^_' }] }
+  }
 ];
